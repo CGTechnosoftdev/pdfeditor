1ccda9c1f1fcced34e05e86310a209b36ca3b86c
Akash Sharma <Akash.Sharma@matellio.com>
2020-11-04 11:56:02 +0530

    minor style changes

--- resources/views/admin/partials/back-button.blade.php
+++ resources/views/admin/partials/back-button.blade.php
@@ -1,5 +1,5 @@
 @if(!empty($back_button))
-<a class="btn btn-default pull-right" href="{{ $back_button['link'] }}">
+<a class="btn btn-default pull-right margin-left-1" href="{{ $back_button['link'] }}">
 	{{ $back_button['label'] }}
 </a>
 @endif
