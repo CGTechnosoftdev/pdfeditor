97f33c18f66c4159bac296a9646f4d613717c941
Pawan Sharma <Pawan.Sharma@matellio.com>
2020-12-04 17:48:45 +0530

    PDF writer/editor change

--- public/pdfannotate.js
+++ public/pdfannotate.js
@@ -13,6 +13,7 @@ var PDFAnnotate = function (container_id, url, options = {}) {
 	this.borderColor = '#000000';
 	this.borderSize = 1;
 	this.font_size = 16;
+	this.fontFamily = "Comic Sans MS, Comic Sans, cursive";
 	this.active_canvas = 0;
 	this.container_id = container_id;
 	this.url = url;
@@ -49,6 +50,7 @@ var PDFAnnotate = function (container_id, url, options = {}) {
 			});
 		}
 	}, function (reason) {
+		console.log(reason);
 	});
 
 	this.initFabric = function () {
@@ -138,6 +140,7 @@ var PDFAnnotate = function (container_id, url, options = {}) {
 				fill: inst.color,
 				fontSize: inst.font_size,
 				selectable: true,
+				fontFamily: inst.fontFamily,
 				fontWeight: inst.fontWeight
 			});
 			fabricObj.add(text);
@@ -236,7 +239,13 @@ PDFAnnotate.prototype.savePdf = function () {
 		}
 		doc.addImage(fabricObj.toDataURL(), 'png', 0, 0);
 	});
-	doc.save('sample.pdf');
+	var d = new Date();
+	const hr = d.getHours();
+	const min = d.getMinutes();
+	const sec = d.getSeconds();
+	const fileName = "file"+hr+min+sec;
+	localStorage.setItem("pdfFile", fileName);
+	doc.save(fileName+'.pdf');
 }
 
 PDFAnnotate.prototype.setBrushSize = function (size) {
@@ -249,7 +258,7 @@ PDFAnnotate.prototype.setBrushSize = function (size) {
 PDFAnnotate.prototype.setColor = function (color) {
 	var inst = this;
 	inst.color = color;
-	var fabricObj = inst.fabricObjects[0]
+	var fabricObj = inst.fabricObjects[inst.active_canvas];
 	var o = inst.fabricObjects[inst.active_canvas].getActiveObject();
 	o.set({ fill: color });
 	fabricObj.renderAll().setActiveObject(o);
@@ -262,6 +271,10 @@ PDFAnnotate.prototype.setBorderColor = function (color) {
 
 PDFAnnotate.prototype.setFontSize = function (size) {
 	this.font_size = size;
+}
+
+PDFAnnotate.prototype.setFontFamily = function(fontFamily){
+	this.fontFamily = fontFamily;
 }
 
 PDFAnnotate.prototype.setBorderSize = function (size) {
@@ -315,6 +328,76 @@ PDFAnnotate.prototype.enableAddImage = function () {
 	});
 }
 
+PDFAnnotate.prototype.addSignature = function(url){
+	var inst = this;
+	var fabricObj = inst.fabricObjects[inst.active_canvas];
+	var canvas = document.createElement('canvas');
+	new fabric.Image.fromURL(url, function (img) {
+		img.set({
+			id: 'image_' + 1,
+			width: canvas.width / 2,
+			height: canvas.height / 2
+		});
+		fabricObj.add(img).renderAll().setActiveObject(img);
+	});
+}
+
+PDFAnnotate.prototype.addDrawSignature = function(url){
+	var inst = this;
+	var fabricObj = inst.fabricObjects[inst.active_canvas];
+	var canvas = document.createElement('canvas');
+	new fabric.Image.fromURL(url, function (img) {
+		img.set({
+			id: 'image_' + 1,
+			width: canvas.width / 2,
+			height: canvas.height / 2
+		});
+		fabricObj.add(img).renderAll().setActiveObject(img);
+	});
+	
+}
+
+PDFAnnotate.prototype.addCrossIcon = function(){
+	var inst = this;
+	var fabricObj = inst.fabricObjects[inst.active_canvas];
+	var canvas = document.createElement('canvas');
+	new fabric.Image.fromURL("./times-solid.png", function (img) {
+		img.set({
+			id: 'image_' + 1,
+			width: canvas.width / 4,
+			height: canvas.height / 4
+		});
+		fabricObj.add(img).renderAll().setActiveObject(img);
+	});
+}
+
+PDFAnnotate.prototype.addCircleIcon = function(){
+	var inst = this;
+	var fabricObj = inst.fabricObjects[inst.active_canvas];
+	var canvas = document.createElement('canvas');
+	new fabric.Image.fromURL("./circle-regular.png", function (img) {
+		img.set({
+			id: 'image_' + 1,
+			width: canvas.width / 4,
+			height: canvas.height / 4
+		});
+		fabricObj.add(img).renderAll().setActiveObject(img);
+	});
+}
+
+PDFAnnotate.prototype.addCheckIcon = function(){
+	var inst = this;
+	var fabricObj = inst.fabricObjects[inst.active_canvas];
+	var canvas = document.createElement('canvas');
+	new fabric.Image.fromURL("./check-solid.png", function (img) {
+		img.set({
+			id: 'image_' + 1,
+			width: canvas.width / 4,
+			height: canvas.height / 4
+		});
+		fabricObj.add(img).renderAll().setActiveObject(img);
+	});
+}
 
 
 PDFAnnotate.prototype.loadFromJSON = function (jsonData) {
@@ -326,4 +409,119 @@ PDFAnnotate.prototype.loadFromJSON = function (jsonData) {
 			})
 		}
 	})
+}
+
+PDFAnnotate.prototype.addPage = function(){
+	var inst = this;
+	inst.number_of_pages = inst.number_of_pages+1;
+	const current_cans = $(".canvas-container").length;
+	var loadingTask = PDFJS.getDocument("./blank.pdf");
+	loadingTask.promise.then(function (pdf) {
+		var scale = 1.3;
+		for (let i = 1; i <=1 ; i++){
+			pdf.getPage(1).then(function (page) {
+				var viewport = page.getViewport(scale);
+				var canvas = document.createElement('canvas');
+				document.getElementById(inst.container_id).appendChild(canvas);
+				canvas.className = 'pdf-canvas';
+				canvas.height = viewport.height;
+				canvas.width = viewport.width;
+				const newidex= parseInt(current_cans)+1;
+				canvas.id = "page-"+newidex+"-canvas";
+				context = canvas.getContext('2d');
+
+				var renderContext = {
+					canvasContext: context,
+					viewport: viewport
+				};
+				var renderTask = page.render(renderContext);
+					options = {};
+				renderTask.then(function () {
+					inst.pages_rendered++;
+					if (inst.pages_rendered == inst.number_of_pages){
+						var el = $('#' + inst.container_id + ' canvas').last();
+						$(el).each(function (index, el) {
+							var background = el.toDataURL("image/png");
+							var fabricObj = new fabric.Canvas(el.id, {
+								freeDrawingBrush: {
+									width: 1,
+									color: inst.color
+								}
+							});
+							inst.fabricObjects.push(fabricObj);
+							var newindex = inst.fabricObjects.length;
+							if (typeof options.onPageUpdated == 'function') {
+								fabricObj.on('object:added', function () {
+									var oldValue = Object.assign({}, inst.fabricObjectsData[newindex-1]);
+									inst.fabricObjectsData[newindex-1] = fabricObj.toJSON()
+									options.onPageUpdated(newindex-1 + 1, oldValue, inst.fabricObjectsData[newindex-1])
+								})
+							}
+							fabricObj.setBackgroundImage(background, fabricObj.renderAll.bind(fabricObj));
+							$(fabricObj.upperCanvasEl).click(function (event) {
+								inst.active_canvas = newindex-1;
+								inst.fabricClickHandler(event, fabricObj);
+							});
+							fabricObj.on('after:render', function () {
+								inst.fabricObjectsData[newindex-1] = fabricObj.toJSON()
+								fabricObj.off('after:render')
+							})
+						});
+					}
+				});
+			});
+		}
+	}, function (reason) {
+		console.log(reason);
+	});
+}
+
+PDFAnnotate.prototype.rotatePage = function(){
+	var inst = this;
+	var o = inst.fabricObjects[inst.active_canvas];
+	const rotationValue = o.wrapperEl.style.transform;
+	if(rotationValue){
+		const currentRotation = rotationValue.substring(
+			rotationValue.lastIndexOf("(") + 1, 
+			rotationValue.lastIndexOf("deg)")
+		);
+		let newValue = parseInt(currentRotation)+90;
+		if(newValue == 360){
+			newValue = 0;
+		}
+		o.wrapperEl.style.transform = "rotate("+newValue+"deg)";
+	} else {
+		o.wrapperEl.style.transform = "rotate(90deg)";
+	}
+	
+}
+
+PDFAnnotate.prototype.addDate = function(event){
+	var inst = this;
+	var fabricObj = inst.fabricObjects[inst.active_canvas];
+	var today = new Date();
+	var dd = String(today.getDate()).padStart(2, '0');
+	var mm = String(today.getMonth() + 1).padStart(2, '0');
+	var yyyy = today.getFullYear();
+	today = mm + '/' + dd + '/' + yyyy;
+	var text = new fabric.IText(today, {
+		fill: inst.color,
+		fontSize: 16,
+		selectable: true,
+		fontFamily: inst.fontFamily,
+		fontWeight: inst.fontWeight
+	});
+	fabricObj.add(text).renderAll().setActiveObject(text);
+}
+
+PDFAnnotate.prototype.deletePage = function(){
+	var inst = this;
+	var activePage = inst.fabricObjects[inst.active_canvas];
+		pageId = activePage.lowerCanvasEl.getAttribute('id');
+		pageId = pageId.match("page-(.*)-canvas")[1];
+		inst.fabricObjectsData.splice(inst.active_canvas, 1);
+	if (activePage) {
+		if (confirm('Are you sure you want to delete page number '+pageId+' ?')) activePage.wrapperEl.remove();
+	}
+	
 }
