1d9431f102d895ed9b7509bef01b28e83a1c6c2a
Sunil Saini <Sunil.Saini@matellio.com>
2020-12-18 10:39:07 +0530

    extra code remove

--- resources/views/admin/partials/back-button.blade.php
+++
@@ -1,5 +0,0 @@
-@if(!empty($back_button))
-<a class="btn btn-default pull-right margin-left-1" href="{{ $back_button['link'] }}">
-	{{ $back_button['label'] }}
-</a>
-@endif
--- resources/views/front/front-home.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.front-home')
-@section("content")
-
-@include('front.partials.front-middle-section')
-
-@include('front.blocks.solve-pdf-problems')
-
-@endsection
