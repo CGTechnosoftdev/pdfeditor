273ecb9d081b7fd40aec2b94805f683b9654a383
Sunil Saini <Sunil.Saini@matellio.com>
2021-01-07 20:07:43 +0530

    trash page design

--- public/front/js/custom.js
+++ public/front/js/custom.js
@@ -8,24 +8,24 @@ function unblockUI() {
     $.unblockUI();
 }
 // Copy
-var clipboardDemos = new Clipboard('[data-clipboard-demo]');
+// var clipboardDemos = new Clipboard('[data-clipboard-demo]');
 
-clipboardDemos.on('success', function(e) {
-    e.clearSelection();
+// clipboardDemos.on('success', function(e) {
+//     e.clearSelection();
 
-    console.info('Action:', e.action);
-    console.info('Text:', e.text);
-    console.info('Trigger:', e.trigger);
+//     console.info('Action:', e.action);
+//     console.info('Text:', e.text);
+//     console.info('Trigger:', e.trigger);
 
-    showTooltip(e.trigger, 'Copied!');
-});
+//     showTooltip(e.trigger, 'Copied!');
+// });
 
-clipboardDemos.on('error', function(e) {
-    console.error('Action:', e.action);
-    console.error('Trigger:', e.trigger);
+// clipboardDemos.on('error', function(e) {
+//     console.error('Action:', e.action);
+//     console.error('Trigger:', e.trigger);
 
-    showTooltip(e.trigger, fallbackMessage(e.action));
-});
+//     showTooltip(e.trigger, fallbackMessage(e.action));
+// });
 
 // tooltips.js
 
